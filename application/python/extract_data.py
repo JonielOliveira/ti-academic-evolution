af6d400348f10568941b587a273bb732e673baa3
Joniel R. de Oliveira <joniel.oliveira@fatec.sp.gov.br>
2025-05-28 19:12:08 -0300

    :sparkles: refactor: remove redundant file path and JSON handling functions

--- application/python/extract_data.py
+++ application/python/extract_data.py
@@ -1,53 +1,8 @@
-import json
-import os
 import requests
 from bs4 import BeautifulSoup
 import re
 import copy
-
-def get_file_path(filename):
-    """Retorna o caminho absoluto de um arquivo localizado na mesma pasta do script .py."""
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(script_dir, filename)
-
-
-def carregar_json(arquivo_json):
-    """
-    Carrega e retorna o conteúdo de um arquivo JSON.
-
-    Parâmetros:
-    - arquivo_json (str): nome do arquivo JSON.
-
-    Retorna:
-    - O conteúdo carregado (normalmente um dict ou list).
-    """
-    try:
-        with open(get_file_path(arquivo_json), 'r', encoding='utf-8') as f:
-            conteudo = json.load(f)
-        return conteudo
-    except Exception as e:
-        print(f"Erro ao ler o arquivo JSON '{arquivo_json}': {e}")
-        return None
-
-
-def salvar_json(conteudo, arquivo_json):
-    """
-    Salva o conteúdo fornecido em um arquivo JSON.
-
-    Parâmetros:
-    - conteudo: objeto Python (dict, list, etc.) a ser salvo.
-    - arquivo_json (str): nome do arquivo JSON de destino.
-
-    Retorna:
-    - None.
-    """
-    try:
-        with open(get_file_path(arquivo_json), 'w', encoding='utf-8') as f:
-            json.dump(conteudo, f, ensure_ascii=False, indent=4)
-        print(f"Arquivo JSON salvo com sucesso: {arquivo_json}")
-    except Exception as e:
-        print(f"Erro ao salvar o arquivo JSON '{arquivo_json}': {e}")
-
+from utils_io import carregar_json, salvar_json
 
 def extrair_dados_iniciais(arquivo_entrada_json, arquivo_saida_json):
     """
