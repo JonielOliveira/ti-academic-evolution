5269b09877aaa4bd5b47c71042cb07ba5e56f160
Joniel R. de Oliveira <joniel.oliveira@fatec.sp.gov.br>
2025-05-29 17:47:30 -0300

    :sparkles: feat: refactor data loading and processing to support environment-based configuration

--- application/python/app.py
+++ application/python/app.py
@@ -1,14 +1,29 @@
 from utils_io import carregar_json, salvar_json
 from extract_data import extrair_dados_iniciais, extrair_dados_lote, verificar_completude_json
+from keyword_inferencer import processar_lista_para_keywords
 from transform_data import processar_dados_complementares
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+# Define qual ambiente usar (via variável do sistema: development | production)
+ENV = os.getenv("ENV", "production")
 
-extrair_dados_iniciais('config/sample_sources.json', 'sample/dados_iniciais.json')
+# Carrega os dados iniciais a partir do arquivo de configuração
+file_path = 'sample' if ENV == 'development' else 'data'
 
-template = carregar_json('config/template.json')
-extrair_dados_lote(template, 'sample/dados_iniciais.json','sample/dados_completos.json')
+template_filepath = 'config/template.json'
+sources_filepath = 'config/sample_sources.json' if ENV == 'development' else 'config/sources.json'
+dados_iniciais_filepath = f'{file_path}/{"dados_iniciais.json"}'
+dados_completos_filepath = f'{file_path}/{"dados_completos.json"}'
+keywords_filepath = f'{file_path}/{"keywords.json"}'
+dados_ajustados_filepath = f'{file_path}/{"dados_ajustados.json"}'
 
-dados = carregar_json('sample/dados_completos.json')
+extrair_dados_iniciais(sources_filepath, dados_iniciais_filepath)
+template = carregar_json(template_filepath)
+extrair_dados_lote(template, dados_iniciais_filepath, dados_completos_filepath)
+dados = carregar_json(dados_completos_filepath)
 verificar_completude_json(dados, template)
-
-dados_enriquecidos = processar_dados_complementares(dados, 'sample/dados_ajustados.json')
+processar_lista_para_keywords(dados, keywords_filepath)
+keywords = carregar_json(keywords_filepath)
+dados_enriquecidos = processar_dados_complementares(dados, keywords, dados_ajustados_filepath)
